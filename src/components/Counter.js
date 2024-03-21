c4139e517cec45c41c42496b6b05f920770caa1c
hatef khodkar <hatef.khodkar@hotmail.com>
2024-03-21 13:07:26 +0330

    feat(redux counter): use redux store and handle increment and decrement in counter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,29 @@
 import classes from './Counter.module.css';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+
+  const counter = useSelector(state => state.counterReducer.counter)
+  const dispatch = useDispatch()
+
+  const toggleCounterHandler = () => { };
+
+  function handleIncrement() {
+    dispatch({ type: 'increment' });
+  }
+
+  function handleDecrement() {
+    dispatch({ type: 'decrement' });
+  }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>-- {counter} --</div>
+      <div>
+        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleDecrement}>decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
