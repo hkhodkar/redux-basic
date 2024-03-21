e134e59cf2d1eecc0aa81454feff64ca98924cb6
hatef khodkar <hatef.khodkar@hotmail.com>
2024-03-21 14:06:45 +0330

    refactor(Counter) refactor counter with redux-toolkit slice

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,29 +1,51 @@
+import { useState } from 'react';
 import classes from './Counter.module.css';
 import { useSelector, useDispatch } from 'react-redux';
+import {counterActions} from '../store/index'
 
 const Counter = () => {
 
-  const counter = useSelector(state => state.counterReducer.counter)
+  const [increaseBy, setIncreaseBy] = useState(2);
+  const counter = useSelector(state => state.counter.counter);
+  const toggleCounter = useSelector(state => state.counter.toggleCounter);
   const dispatch = useDispatch()
 
-  const toggleCounterHandler = () => { };
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+   };
 
   function handleIncrement() {
-    dispatch({ type: 'increment' });
+    dispatch(counterActions.increment());
   }
 
   function handleDecrement() {
-    dispatch({ type: 'decrement' });
+    dispatch(counterActions.decrement());
+  }
+
+  function handleOnChangeIncrement(event) {
+    setIncreaseBy(event.target.value);
+  }
+
+  function handleIncreaseBy() {
+    dispatch(counterActions.increaseBy(+increaseBy))
   }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>-- {counter} --</div>
-      <div>
-        <button onClick={handleIncrement}>Increment</button>
-        <button onClick={handleDecrement}>decrement</button>
-      </div>
+      { toggleCounter && 
+        <>
+            <div>
+              <button onClick={handleIncrement}>Increment</button>
+              <button onClick={handleDecrement}>decrement</button>
+            </div>
+            <div>
+              <button onClick={handleIncreaseBy}>Increase by {increaseBy}</button>
+              <input type="number" value={increaseBy} onChange={handleOnChangeIncrement} />
+            </div>
+        </>
+      }
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
